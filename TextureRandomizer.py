864138a606ce26754216478955962af5f8dbabed
Exy-mat <albin.morel@laposte.net>
2021-07-17 14:11:01 +0200

    Update TextureRandomizer.py

--- TextureRandomizer.py
+++ TextureRandomizer.py
@@ -4,11 +4,11 @@ def main():
     # Blacklist for the file types.
     Blacklisted_file_type = ".mcmeta"
 
-    # Blacklist ofr the textures. Format : "block_texture_name.png". Commas in between block textures.
+    # Blacklist for the textures. Format : "texture_name.png". Commas in between textures.
     Blacklisted_blocks_textures = ["campfire_fire.png","fire_0.png","fire_1.png","kelp.png","kelp_plant.png"]
     Blacklisted_items_textures = []
     
-    #Selector for the subfolder of the textures to be randomized.
+    # Selector for the subfolder of the textures to be randomized.
     str_sub = ""
     subfolders = [f.path for f in os.scandir("textures\\") if f.is_dir()]
     for i in range(len(subfolders)):
@@ -19,9 +19,9 @@ def main():
             select_sub = int(input("Select the number of the texture subfolder you want to use (max "+ str(len(subfolders)-1) + "): " + str_sub+"\n"))
             basepath = subfolders[select_sub]
             break
-        except ValueError: #If user doesn't use a number
+        except ValueError: # If user doesn't use a number
             print("Please use a number to select the wanted subfolder")
-        except IndexError: #If user use a bigger number than possible or a negative number
+        except IndexError: # If user use a bigger number than possible or a negative number
             print("The subfolder doesn't exist or you have mistyped the number")
     
     # Creates a list of all the textures in the specified textures folder, without the blacklisted ones.
@@ -44,12 +44,12 @@ def main():
     new_block_textures_list = block_textures_list.copy()
     random.shuffle(new_block_textures_list)
 
-    # Creates the new ressources pack directory and paste the pck.mcmeta
+    # Creates the new ressource pack directory and folder and paste the pack.mcmeta in the new ressource pack
     dirName = "new_ressources\\" + new_pack_name + "\\assets\\minecraft\\textures\\block"
     os.makedirs(dirName)
     Add_pack = shutil.copy("pack.mcmeta", "new_ressources\\" + new_pack_name + "\\")
 
-    # Creates the new ressources pack by associting the blocks to their new textures
+    # Creates the new ressource pack by associating the blocks to their new textures
     for old,new in zip(block_textures_list,new_block_textures_list):
         new_block_texture = shutil.copy(basepath+"\\"+old, dirName+"\\"+new)
 
