d2960ddbcc87c6938d6786df006aa80f9c857a04
Exy-mat <albin.morel@laposte.net>
2021-07-18 13:42:47 +0200

    Update TexturesRandomizer.py

--- TexturesRandomizer.py
+++ TexturesRandomizer.py
@@ -1,42 +1,39 @@
 import os, random, shutil, json
 
 def main():
-    # Blacklist for the file types.
-    blacklisted_file_type = [".mcmeta",".jpeg",".jpg",'.gif']
-
     # Blacklist for the textures. Add texture to the blacklist in blacklist.txt.
     blacklist_file = open("blacklist.txt", "r")
     blacklist_textures_list = []
-    
+
     for line in blacklist_file:
         stripped_line = line.strip()
         blacklist_textures_list.append(stripped_line)
 
     blacklist_file.close()
 
+    # Generates a new random name for the ressource pack
+    new_pack_name = "resource_pack_" + str(random.randint(1,1000000))
+
     # Selector for the subfolder of the textures to be randomized.
     subfolder_string_list = ""
     subfolders_list = [f.path for f in os.scandir("textures\\") if f.is_dir()]
     for i in range(len(subfolders_list)):
         subfolder_string_list = subfolder_string_list + subfolders_list[i] + "[" + str(i) + "] "
-    
+
     while True:
         try:
             select_sub = int(input("Select the number of the texture subfolder you want to use (max "+ str(len(subfolders_list)-1) + "): " + subfolder_string_list +"\n"))
             basepath = subfolders_list[select_sub]
             break
         except ValueError:
-            print("Please use a number to select the wanted subfolder.")
+            print("Please use a number to select the subfolder.")
         except IndexError:
             print("The subfolder doesn't exist or you have mistyped the number.")
-    
-    # Generates a new random name for the ressource pack
-    new_pack_name = "resource_pack_" + str(random.randint(1,1000000))
-    
+
     # Creates the new resource pack directory
     new_directory = "new_resources\\" + new_pack_name + "\\assets\\minecraft\\" + basepath
-    os.makedirs(new_directory)    
-    
+    os.makedirs(new_directory)
+
     # Creates a list of all the textures in the specified textures folder, without the blacklisted ones.
     textures_list = []
     with os.scandir(basepath) as entries:
@@ -45,12 +42,12 @@ def main():
                 if entry.name in blacklist_textures_list:
                     blacklist_textures = shutil.copy(basepath + "\\" + entry.name, new_directory + "\\" + entry.name)
                     continue
-                if entry.name.endswith(tuple(blacklisted_file_type)) == True:
+                if entry.name.endswith(".png") != True:
                     blacklist_ends = shutil.copy(basepath + "\\" + entry.name, new_directory + "\\" + entry.name)
                     continue
                 else:
                     textures_list.append(entry.name)
-    
+
     # Shuffle the textures in a new list
     new_textures_list = textures_list.copy()
     random.shuffle(new_textures_list)
@@ -58,7 +55,7 @@ def main():
     # Creates the new resource pack by associting the blocks to their new textures
     for old,new in zip(textures_list,new_textures_list):
         new_textures = shutil.copy(basepath+"\\"+old, new_directory+"\\"+new)
-    
+
     # Selector for the version of the resource pack, for the pack.mcmeta
     while True:
         try:
@@ -75,14 +72,14 @@ def main():
             break
         except ValueError:
             print("Please use a number to select the resource pack version.")
-    
+
     # Adds the selected pack.mcmeta and the cool kid description
     description_text = "This is a randomly generated resource pack"
     random_description_text = ""
     hex_char = [1,2,3,4,5,6,7,8,9,"a","b","c","d","e","f"]
     for i in range(len(description_text)):
         random_description_text += "\u00A7"+ str(random.choice(hex_char)) + description_text[i] + "\u00A7r"
-                                                       
+
     pack_path = "new_resources\\"+ new_pack_name
     pack_name = "pack.mcmeta"
     completePath = os.path.join(pack_path,pack_name)
@@ -95,7 +92,7 @@ def main():
 
     with open(completePath, "w") as pack_create:
         json.dump(pack_format, pack_create, indent=2)
-    
+
     # Ending
     print("Done, you can find your new resource pack : " + new_pack_name)
     os.system('pause')
